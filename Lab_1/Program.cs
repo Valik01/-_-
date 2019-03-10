34a057a5bf221623c6adec61d5e979282af83a1a
Valentyn Pereverten <valentynp133@gmail.com>
2019-03-10 20:09:09 +0200

    _1

---
+++ Lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value a and b");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a > b)
+            {
+                double c = Math.Sqrt(a - b);
+                Console.WriteLine("Squaring the difference a and b equals {0} ", c);
+            }
+            else
+                Console.WriteLine("Тhe root of a negative number is not extracted");
+        }
+    }
+}
